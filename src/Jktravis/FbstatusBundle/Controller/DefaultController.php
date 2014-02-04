778d856494c85c28a21d7bb3524ba7a5ffaf3cd2
jktravis <joshua.travis@gmail.com>
2014-02-03 20:41:15 -0500

    Fbstatus bundle created

---
+++ src/Jktravis/FbstatusBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jktravis\FbstatusBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('JktravisFbstatusBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Jktravis/FbstatusBundle/JktravisFbstatusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jktravis\FbstatusBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JktravisFbstatusBundle extends Bundle
+{
+}
