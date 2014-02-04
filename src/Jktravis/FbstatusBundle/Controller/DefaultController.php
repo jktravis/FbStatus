38e84717235a44b7b126bff353f57885e4cf9e27
jktravis <joshua.travis@gmail.com>
2014-02-03 21:45:06 -0500

    Added annotated routing for "/" to Default controller.

--- src/Jktravis/FbstatusBundle/Controller/DefaultController.php
+++ src/Jktravis/FbstatusBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Jktravis\FbstatusBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('JktravisFbstatusBundle:Default:index.html.twig', array('name' => $name));
-    }
+	/**
+	 * @Route("/")
+	 */
+	public function indexAction($name)
+	{
+		return $this->render('JktravisFbstatusBundle:Default:index.html.twig', array('name' => $name));
+	}
 }
