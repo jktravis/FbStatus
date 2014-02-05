f158aad5c8f295ac1c434bf1aefe70de7e4013f0
jktravis <joshua.travis@gmail.com>
2014-02-04 19:50:42 -0500

    Added named route to DefaultController

--- src/Jktravis/FbstatusBundle/Controller/DefaultController.php
+++ src/Jktravis/FbstatusBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="home")
 	 */
 	public function indexAction()
 	{
